e7d125e9cff64548ca9abf2062e39f674112e1cf
Adrian-DT <adriandt_work@outlook.com>
2026-06-16 10:03:50 +0200

    Add card component

--- projects/demo-01/src/app/core/components/app/app.ts
+++ projects/demo-01/src/app/core/components/app/app.ts
@@ -1,15 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sample } from "../sample/sample";
+import { Card } from '../card/card';
 
 @Component({
   selector: 'ind-root',
-  imports: [RouterOutlet, Sample],
+  imports: [RouterOutlet, Sample, Card],
   template: `
+  <header>
     <h1>Hello, {{ title() }}</h1>
 
+  </header>
+  <main>
     <router-outlet />
-    <ind-sample />
+    <ind-card>
+      <!-- Para mostrar el contenido de ind-card, tenemos que poner en el template de card, el ng-content, si no, no muestra el contenido -->
+      <ind-sample />
+    </ind-card>
+  </main>
+
   `,
   styles: [],
 })
